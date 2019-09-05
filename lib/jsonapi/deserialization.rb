b51216e5d051a1e496aad9d9d8546b638bd5d54d
niklaslilland <niklilland@gmail.com>
2019-09-05 10:57:56 -0700

    Updated deserializer to allow all jsonapi request fields.

--- lib/jsonapi/deserialization.rb
+++ lib/jsonapi/deserialization.rb
@@ -15,50 +15,59 @@ module JSONAPI
     #   except: Array of symbols of blacklisted fields.
     #   polymorphic: Array of symbols of polymorphic fields.
     # @return [Hash]
-    def jsonapi_deserialize(document, options = {})
+    def jsonapi_deserialize(document)
+
+      # Optional fields, if present, convert to snake/symbol
+      # data - (if present, flatten it accordingly to jsonapi spec)
+      # include
+      # fields
+      # filter
+      # sort
+      # page
+      # platform
+
+      # Duplicate document and convert to JSON
       if document.is_a?(ActionController::Parameters)
-        primary_data = document.dup.require(:data).permit!.as_json
+        primary_data = document.dup.as_json
       elsif document.is_a?(Hash)
-        primary_data = (document.as_json['data'] || {}).deep_dup
+        primary_data = document.as_json.deep_dup
       else
         return {}
-      end
-
-      # Transform keys and any option values.
-      options = options.as_json
-      ['only', 'except', 'polymorphic'].each do |opt_name|
-        opt_value = options[opt_name]
-        options[opt_name] = Array(opt_value).map(&:to_s) if opt_value
       end
 
       # Convert primary_data to snake_case
       primary_data.deep_transform_keys!(&:underscore)
 
-      relationships = primary_data['relationships'] || {}
-      parsed = primary_data['attributes'] || {}
-      parsed['id'] = primary_data['id'] if primary_data['id']
+      # This is the hash that is going to be returned
+      parsed = {}
+      parsed['include'] = primary_data['include'] if primary_data['include']
+      parsed['fields'] = primary_data['fields'] if primary_data['fields']
+      parsed['filter'] = primary_data['filter'] if primary_data['filter']
+      parsed['sort'] = primary_data['sort'] if primary_data['sort']
+      parsed['page'] = primary_data['page'] if primary_data['page']
+      parsed['platform'] = primary_data['platform'] if primary_data['platform']
 
-      # Remove unwanted items from a dictionary.
-      if options['only']
-        [parsed, relationships].map { |hsh| hsh.slice!(*options['only']) }
-      elsif options['except']
-        [parsed, relationships].map { |hsh| hsh.except!(*options['except']) }
-      end
+      if primary_data['data']
+        relationships = primary_data['relationships'] || {}
+        parsed['id'] = primary_data['id'] if primary_data['id']
 
-      relationships.map do |assoc_name, assoc_data|
-        assoc_data = (assoc_data || {})['data'] || {}
-        rel_name = ActiveSupport::Inflector.singularize(assoc_name)
-
-        if assoc_data.is_a?(Array)
-          parsed["#{rel_name}_ids"] = assoc_data.map { |ri| ri['id'] }.compact
-          next
+        # Map primary_data['data']['attributes'] to parsed hash
+        if primary_data['data']['attributes'].respond_to? :each
+          primary_data['data']['attributes'].each do |key, val|
+            parsed[key] = val
+          end
         end
 
-        parsed["#{rel_name}_id"] = assoc_data['id']
+        relationships.map do |assoc_name, assoc_data|
+          assoc_data = (assoc_data || {})['data'] || {}
+          rel_name = ActiveSupport::Inflector.singularize(assoc_name)
 
-        if (options['polymorphic'] || []).include?(assoc_name)
-          rel_type = ActiveSupport::Inflector.classify(assoc_data['type'].to_s)
-          parsed["#{rel_name}_type"] = rel_type
+          if assoc_data.is_a?(Array)
+            parsed["#{rel_name}_ids"] = assoc_data.map { |ri| ri['id'] }.compact
+            next
+          end
+
+          parsed["#{rel_name}_id"] = assoc_data['id']
         end
       end
 
